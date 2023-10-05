0284f0434ef2245e0cbb5feffc17ccdac920e697
Maurice Lam <me@mauricelam.com>
2023-10-05 02:26:37 -0700

    Support Saturating shifts with --feature saturating_int_impl

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate proc_macro;
 use anyhow::bail;
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{Expr, ExprBinary, ExprLit, ExprUnary};
+use syn::{Expr, ExprBinary, ExprGroup, ExprLit, ExprParen, ExprUnary};
 
 #[proc_macro]
 pub fn panicking(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -25,17 +25,21 @@ fn panicking_impl(item: TokenStream) -> anyhow::Result<TokenStream> {
                 let new_left = recurse(*left)?;
                 let new_right = recurse(*right)?;
                 checked_op(op)?
-                    .map(|func| quote! { #new_left.#func(#new_right).unwrap() })
+                    .map(|func| quote! { (#new_left).#func(#new_right).unwrap() })
                     .unwrap_or_else(|| quote! { #new_left #op #new_right })
             }
             Expr::Cast(_) => quote! { #expr },
             // Recurse inside the following items
             Expr::Index(_) => quote! { #expr },
             Expr::Lit(_) => quote! { #expr },
-            Expr::Paren(_) => quote! { #expr },
+            Expr::Paren(ExprParen { expr, .. }) => {
+                let new_expr = recurse(*expr)?;
+                quote! { ( #new_expr ) }
+            }
             Expr::Path(_) => quote! { #expr },
             Expr::Reference(_) => quote! { #expr },
             Expr::Unary(_) => quote! { #expr },
+            Expr::Group(ExprGroup { expr, .. }) => recurse(*expr)?,
             _ => bail!("Unexpected expression token {expr:?}"),
         })
     }
@@ -63,17 +67,21 @@ fn wrapping_impl(item: TokenStream) -> anyhow::Result<TokenStream> {
                 let new_left = recurse(*left)?;
                 let new_right = recurse(*right)?;
                 wrapping_op(op)?
-                    .map(|func| quote! { #new_left.#func(#new_right) })
+                    .map(|func| quote! { (#new_left).#func(#new_right) })
                     .unwrap_or_else(|| quote! { #new_left #op #new_right })
             }
             Expr::Cast(_) => quote! { #expr },
             // Recurse inside the following items
             Expr::Index(_) => quote! { #expr },
             Expr::Lit(_) => quote! { #expr },
-            Expr::Paren(_) => quote! { #expr },
+            Expr::Paren(ExprParen { expr, .. }) => {
+                let new_expr = recurse(*expr)?;
+                quote! { ( #new_expr ) }
+            }
             Expr::Path(_) => quote! { #expr },
             Expr::Reference(_) => quote! { #expr },
             Expr::Unary(_) => quote! { #expr },
+            Expr::Group(ExprGroup { expr, .. }) => recurse(*expr)?,
             _ => bail!("Unexpected expression token {expr:?}"),
         })
     }
@@ -100,18 +108,70 @@ fn saturating_impl(item: TokenStream) -> anyhow::Result<TokenStream> {
             }) => {
                 let new_left = recurse(*left)?;
                 let new_right = recurse(*right)?;
-                saturating_op(op)?
-                    .map(|func| quote! { #new_left.#func(#new_right) })
-                    .unwrap_or_else(|| quote! { #new_left #op #new_right })
+                match op {
+                        syn::BinOp::Add(_) => quote! { (#new_left).saturating_add(#new_right) },
+                        syn::BinOp::Sub(_) => quote! { (#new_left).saturating_sub(#new_right) },
+                        syn::BinOp::Mul(_) => quote! { (#new_left).saturating_mul(#new_right) },
+                        syn::BinOp::Div(_) => quote! { (#new_left).saturating_div(#new_right) },
+                        syn::BinOp::Rem(_) => quote! { (#new_left).saturating_rem(#new_right) },
+                        syn::BinOp::Shl(_) => {
+                            #[cfg(feature = "saturating_int_impl")]
+                            {
+                                quote! { (::core::num::Saturating(#new_left) #op ::core::num::Saturating(#new_right)).0 }
+                            }
+
+                            #[cfg(not(feature = "saturating_int_impl"))]
+                            {
+                                bail!("Saturating bit shifts are not supported (https://github.com/rust-lang/libs-team/issues/230)")
+                            }
+                        },
+                        syn::BinOp::Shr(_) => {
+                            #[cfg(feature = "saturating_int_impl")]
+                            {
+                                quote! { (::core::num::Saturating(#new_left) #op ::core::num::Saturating(#new_right)).0 }
+                            }
+
+                            #[cfg(not(feature = "saturating_int_impl"))]
+                            {
+                                bail!("Saturating bit shifts are not supported (https://github.com/rust-lang/libs-team/issues/230)")
+                            }
+                        },
+                        syn::BinOp::And(_)
+                        | syn::BinOp::Or(_)
+                        | syn::BinOp::BitXor(_)
+                        | syn::BinOp::BitAnd(_)
+                        | syn::BinOp::BitOr(_)
+                        | syn::BinOp::Eq(_)
+                        | syn::BinOp::Lt(_)
+                        | syn::BinOp::Le(_)
+                        | syn::BinOp::Ne(_)
+                        | syn::BinOp::Ge(_)
+                        | syn::BinOp::Gt(_) => quote! { #new_left #op #new_right },
+                        syn::BinOp::AddAssign(_)
+                        | syn::BinOp::SubAssign(_)
+                        | syn::BinOp::MulAssign(_)
+                        | syn::BinOp::DivAssign(_)
+                        | syn::BinOp::RemAssign(_)
+                        | syn::BinOp::BitXorAssign(_)
+                        | syn::BinOp::BitAndAssign(_)
+                        | syn::BinOp::BitOrAssign(_)
+                        | syn::BinOp::ShlAssign(_)
+                        | syn::BinOp::ShrAssign(_) => bail!("Unsupported operation"),
+                        _ => bail!("Unknown op: {op:?}"),
+                    }
             }
             Expr::Cast(_) => quote! { #expr },
             // Recurse inside the following items
             Expr::Index(_) => quote! { #expr },
             Expr::Lit(_) => quote! { #expr },
-            Expr::Paren(_) => quote! { #expr },
+            Expr::Paren(ExprParen { expr, .. }) => {
+                let new_expr = recurse(*expr)?;
+                quote! { ( #new_expr ) }
+            }
             Expr::Path(_) => quote! { #expr },
             Expr::Reference(_) => quote! { #expr },
             Expr::Unary(_) => quote! { #expr },
+            Expr::Group(ExprGroup { expr, .. }) => recurse(*expr)?,
             _ => bail!("Unexpected expression token {expr:?}"),
         })
     }
@@ -139,20 +199,26 @@ fn checked_impl(item: TokenStream) -> anyhow::Result<TokenStream> {
                 let new_left = recurse(*left)?;
                 let new_right = recurse(*right)?;
                 checked_op(op)?
-                    .map(|func| quote! { #new_left.zip(#new_right).and_then(|(l, r)| l.#func(r)) })
-                    .unwrap_or_else(|| quote! { #new_left.zip(#new_right).map(|(l, r)| l #op r) })
+                    .map(
+                        |func| quote! { (#new_left).zip(#new_right).and_then(|(l, r)| l.#func(r)) },
+                    )
+                    .unwrap_or_else(|| quote! { (#new_left).zip(#new_right).map(|(l, r)| l #op r) })
             }
             Expr::Cast(_) => quote! { #expr },
             // Recurse inside the following items
             Expr::Index(_) => quote! { #expr },
             Expr::Lit(ExprLit { lit, .. }) => quote! { Some(#lit) },
-            Expr::Paren(_) => quote! { #expr },
+            Expr::Paren(ExprParen { expr, .. }) => {
+                let new_expr = recurse(*expr)?;
+                quote! { ( #new_expr ) }
+            }
             Expr::Path(_) => quote! { #expr },
             Expr::Reference(_) => quote! { #expr },
             Expr::Unary(ExprUnary { op, expr, .. }) => {
                 let new_expr = recurse(*expr)?;
-                quote! { #new_expr.map(|v| #op v) }
-            },
+                quote! { (#new_expr).map(|v| #op v) }
+            }
+            Expr::Group(ExprGroup { expr, .. }) => recurse(*expr)?,
             _ => bail!("Unexpected expression token {expr:?}"),
         })
     }
@@ -167,13 +233,13 @@ fn checked_op(op: syn::BinOp) -> anyhow::Result<Option<TokenStream>> {
         syn::BinOp::Mul(_) => Some(quote! { checked_mul }),
         syn::BinOp::Div(_) => Some(quote! { checked_div }),
         syn::BinOp::Rem(_) => Some(quote! { checked_rem }),
+        syn::BinOp::Shl(_) => Some(quote! { checked_shl }),
+        syn::BinOp::Shr(_) => Some(quote! { checked_shr }),
         syn::BinOp::And(_)
         | syn::BinOp::Or(_)
         | syn::BinOp::BitXor(_)
         | syn::BinOp::BitAnd(_)
         | syn::BinOp::BitOr(_)
-        | syn::BinOp::Shl(_)
-        | syn::BinOp::Shr(_)
         | syn::BinOp::Eq(_)
         | syn::BinOp::Lt(_)
         | syn::BinOp::Le(_)
@@ -201,47 +267,13 @@ fn wrapping_op(op: syn::BinOp) -> anyhow::Result<Option<TokenStream>> {
         syn::BinOp::Mul(_) => Some(quote! { wrapping_mul }),
         syn::BinOp::Div(_) => Some(quote! { wrapping_div }),
         syn::BinOp::Rem(_) => Some(quote! { wrapping_rem }),
+        syn::BinOp::Shl(_) => Some(quote! { wrapping_shl }),
+        syn::BinOp::Shr(_) => Some(quote! { wrapping_shr }),
         syn::BinOp::And(_)
         | syn::BinOp::Or(_)
         | syn::BinOp::BitXor(_)
         | syn::BinOp::BitAnd(_)
         | syn::BinOp::BitOr(_)
-        | syn::BinOp::Shl(_)
-        | syn::BinOp::Shr(_)
-        | syn::BinOp::Eq(_)
-        | syn::BinOp::Lt(_)
-        | syn::BinOp::Le(_)
-        | syn::BinOp::Ne(_)
-        | syn::BinOp::Ge(_)
-        | syn::BinOp::Gt(_) => None,
-        syn::BinOp::AddAssign(_)
-        | syn::BinOp::SubAssign(_)
-        | syn::BinOp::MulAssign(_)
-        | syn::BinOp::DivAssign(_)
-        | syn::BinOp::RemAssign(_)
-        | syn::BinOp::BitXorAssign(_)
-        | syn::BinOp::BitAndAssign(_)
-        | syn::BinOp::BitOrAssign(_)
-        | syn::BinOp::ShlAssign(_)
-        | syn::BinOp::ShrAssign(_) => bail!("Unsupported operation"),
-        _ => bail!("Unknown op: {op:?}"),
-    })
-}
-
-fn saturating_op(op: syn::BinOp) -> anyhow::Result<Option<TokenStream>> {
-    Ok(match op {
-        syn::BinOp::Add(_) => Some(quote! { saturating_add }),
-        syn::BinOp::Sub(_) => Some(quote! { saturating_sub }),
-        syn::BinOp::Mul(_) => Some(quote! { saturating_mul }),
-        syn::BinOp::Div(_) => Some(quote! { saturating_div }),
-        syn::BinOp::Rem(_) => Some(quote! { saturating_rem }),
-        syn::BinOp::And(_)
-        | syn::BinOp::Or(_)
-        | syn::BinOp::BitXor(_)
-        | syn::BinOp::BitAnd(_)
-        | syn::BinOp::BitOr(_)
-        | syn::BinOp::Shl(_)
-        | syn::BinOp::Shr(_)
         | syn::BinOp::Eq(_)
         | syn::BinOp::Lt(_)
         | syn::BinOp::Le(_)
--- tests/test.rs
+++ tests/test.rs
@@ -1,30 +1,35 @@
-use arithmetic_mode::{panicking, wrapping, saturating, checked};
+#![no_std]
+
+use arithmetic_mode::{checked, panicking, saturating, wrapping};
+use paste::paste;
 
 #[test]
-fn test_panicking_u8_add_u8() {
+fn test_panicking() {
     assert_eq!(15, panicking! { 5_u8 + 10_u8 });
-}
-
-#[test]
-#[should_panic]
-fn test_panicking_u8_add_u8_overflow() {
-    panicking! { 5_u8 + 255_u8 };
-}
-
-#[test]
-#[should_panic]
-fn test_panicking_u8_add_u8_2() {
-    panicking! { 200_u8 + 30_u8 + 30_u8 };
-}
-
-#[test]
-fn test_panicking_operator_precedence() {
     assert_eq!(11, panicking! { 1_u8 + 2_u8 * 3_u8 + 4_u8 });
 }
 
 #[test]
-fn test_wrapping_add() {
+#[should_panic]
+fn test_u8_add_u8() {
+    panicking! { 5_u8+255_u8 };
+}
+
+#[test]
+#[should_panic]
+fn test_u8_add_u8_2() {
+    panicking! { 200_u8+30_u8+30_u8 };
+}
+
+#[test]
+fn test_wrapping() {
     assert_eq!(15, wrapping! { 5_u8 + 10_u8 });
+    assert_eq!(11, wrapping! { 1_u8 + 2_u8 * 3_u8 + 4_u8 });
+    assert_eq!(1, wrapping! { -1_i8 + 2 });
+    assert_eq!(-128, wrapping! { -1i8 << 7 });
+    assert_eq!(-1, wrapping! { -1i8 << 128 });
+    assert_eq!(-1, wrapping! { -128_i8 >> 7 });
+    assert_eq!(-128, wrapping! { -128_i16 >> 64 });
 }
 
 #[test]
@@ -32,11 +37,6 @@ fn test_wrapping_add_overflow() {
     assert_eq!(4, wrapping! { 5_u8 + 255_u8 });
     assert_eq!(4, wrapping! { 200_u8 + 30_u8 + 30_u8 });
     assert_eq!(u32::MAX, wrapping! { 0_u32 - 1 });
-}
-
-#[test]
-fn test_wrapping_operator_precedence() {
-    assert_eq!(11, wrapping! { 1_u8 + 2_u8 * 3_u8 + 4_u8 });
 }
 
 #[test]
@@ -73,29 +73,35 @@ fn test_checked_operator_precedence() {
     assert_eq!(Some(11), checked! { 1_u8 + 2_u8 * 3_u8 + 4_u8 });
 }
 
-#[test]
-fn test_unchanged_expressions() {
-    assert_eq!(42, panicking! { 42 });
-    assert_eq!(-42, panicking! { -42 });
-    assert_eq!(3, panicking! { 1 | 2 });
-    assert_eq!(3, panicking! { 1 ^ 2 });
-    assert_eq!(2, panicking! { -10 & 2 });
+macro_rules! test_unchanging {
+    ($ident:ident, $expr:expr) => {
+        paste! {
+            #[test]
+            fn [<test_unchanging_panicking_ $ident>]() {
+                assert_eq!(($expr), (panicking! { $expr }));
+            }
 
-    assert_eq!(42, wrapping! { 42 });
-    assert_eq!(-42, wrapping! { -42 });
-    assert_eq!(3, wrapping! { 1 | 2 });
-    assert_eq!(3, wrapping! { 1 ^ 2 });
-    assert_eq!(2, wrapping! { -10 & 2 });
+            #[test]
+            fn [<test_unchanging_wrapped_ $ident>]() {
+                assert_eq!($expr, wrapping!{ $expr });
+            }
 
-    assert_eq!(42, saturating! { 42 });
-    assert_eq!(-42, saturating! { -42 });
-    assert_eq!(3, saturating! { 1 | 2 });
-    assert_eq!(3, saturating! { 1 ^ 2 });
-    assert_eq!(2, saturating! { -10 & 2 });
+            #[test]
+            fn [<test_unchanging_saturating_ $ident>]() {
+                assert_eq!($expr, saturating!{ $expr });
+            }
 
-    assert_eq!(Some(42), checked! { 42 });
-    assert_eq!(Some(-42), checked! { -42 });
-    assert_eq!(Some(3), checked! { 1 | 2 });
-    assert_eq!(Some(3), checked! { 1 ^ 2 });
-    assert_eq!(Some(2), checked! { -10 & 2 });
+            #[test]
+            fn [<test_unchanging_checked_ $ident>]() {
+                assert_eq!(Some($expr), checked!{ $expr });
+            }
+        }
+    };
 }
+
+test_unchanging!(constant, 42);
+test_unchanging!(negate, -42);
+test_unchanging!(or, 1 | 2);
+test_unchanging!(and, -10 & 2);
+test_unchanging!(xor, 1 ^ 2);
+test_unchanging!(complex, (1 ^ 2) | (3_i32 + (-4_i32 * 5_i32)));
